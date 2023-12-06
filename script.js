9fe717d7f10f5fa445d0bedeba679f7ca1d91338
varshini-34 <153053137+varshini-34@users.noreply.github.com>
2023-12-06 10:48:07 +0530

    Add files via upload
    web workshop file

---
+++ script.js
@@ -0,0 +1,5 @@
+        // // JavaScript function to toggle the background color of the home section
+        // function toggleBackgroundColor() {
+        //     var homeSection = document.getElementById('home');
+        //     homeSection.style.backgroundColor = homeSection.style.backgroundColor === 'rgba(0, 0, 0, 0.5)' ? '#333' : 'rgba(0, 0, 0, 0.5)';
+        // }
